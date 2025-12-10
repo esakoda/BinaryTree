19d8e34fece6de0f9863a161887118d876d14e78
ellensakoda <ellen.sakoda@menloschool.org>
2025-12-10 12:58:25 -0800

    Finished insert method

--- src/BST.java
+++ src/BST.java
@@ -48,6 +48,27 @@ public class BST {
      */
     public boolean search(int val) {
         // TODO: Complete the search function
+        return searchHelper(val, root);
+    }
+
+    // Search helper function
+    public boolean searchHelper(int val, BSTNode root){
+        // Base case 1: if the val is not in the tree
+        if (root == null){
+            return false;
+        }
+        // Base case 2: if val is equal to the root value of this smaller tree
+        if (val == root.getVal()){
+            return true;
+        }
+        // If the value is less than the root - go to the left
+        else if (val < root.getVal()){
+            return searchHelper(val, root.getLeft());
+        }
+        // If the value is greater than the root - go to the right
+        else if (val > root.getVal()){
+            return searchHelper(val, root.getRight());
+        }
         return false;
     }
 
@@ -56,7 +77,23 @@ public class BST {
      */
     public ArrayList<BSTNode> getInorder() {
         // TODO: Complete inorder traversal
-        return null;
+        ArrayList<BSTNode> order = new ArrayList<>();
+        inOrderHelper(root, order);
+        return order;
+    }
+
+    // In order helper function
+    public void inOrderHelper(BSTNode node, ArrayList<BSTNode> order){
+        // If there is no node then nothing to do
+        if (node == null){
+            return;
+        }
+        // Trust that it will return inorder of left side
+        inOrderHelper(node.getLeft(), order);
+        // Get root
+        order.add(node);
+        // Trust that it will return inorder of right side
+        inOrderHelper(node.getRight(), order);
     }
 
     /**
@@ -64,7 +101,22 @@ public class BST {
      */
     public ArrayList<BSTNode> getPreorder() {
         // TODO: Complete preorder traversal
-        return null;
+        ArrayList<BSTNode> order = new ArrayList<>();
+        preOrderHelper(root, order);
+        return order;
+    }
+
+    // Preorder helper
+    public void preOrderHelper(BSTNode node, ArrayList<BSTNode> order){
+        if (node == null){
+            return;
+        }
+        // Get root first
+        order.add(node);
+        // Trust that it will return preorder of left side
+        preOrderHelper(node.getLeft(), order);
+        // Trust that it will return preorder of right side
+        preOrderHelper(node.getRight(), order);
     }
 
     /**
@@ -72,9 +124,22 @@ public class BST {
      */
     public ArrayList<BSTNode> getPostorder() {
         // TODO: Complete postorder traversal
-        return null;
+        ArrayList<BSTNode> order = new ArrayList<>();
+        postOrderHelper(root, order);
+        return order;
     }
 
+    public void postOrderHelper(BSTNode node, ArrayList<BSTNode> order){
+        if (node == null){
+            return;
+        }
+        // Trust that it will return postorder of left side
+        postOrderHelper(node.getLeft(), order);
+        // Trust that it will return postorder of right side
+        postOrderHelper(node.getRight(), order);
+        // Add root last
+        order.add(node);
+    }
     /**
      * Inserts the given integer value to the tree
      * if it does not already exist. Modifies the
@@ -83,7 +148,33 @@ public class BST {
      */
     public void insert(int val) {
         // TODO: Complete insert
+        root = insertHelper(val, root);
     }
+
+    public BSTNode insertHelper(int val, BSTNode root){
+        // If the value is already in the tree don't insert
+        if (search(val)){
+            return root;
+        }
+        // If there is no root
+        if (root == null){
+            // Create the new node
+            BSTNode node = new BSTNode(val);
+            return node;
+        }
+        // If val is less than root
+        else if (val < root.getVal()){
+            // Go to the smaller left tree and trust that the function will insert the value correctly
+            root.setLeft(insertHelper(val,root.getLeft()));
+        }
+        // If val is greater than root
+        else if (val > root.getVal()){
+            // Go to the smaller right tree and trust that the function will insert the value correctly
+            root.setRight(insertHelper(val, root.getRight()));
+        }
+        return root;
+    }
+
 
     /**
      * Determines if the current BST is
@@ -122,5 +213,6 @@ public class BST {
         System.out.println("\nInorder traversal of binary tree is");
         sol = tree.getInorder();
         printNodes(sol);
+
     }
 }
